ad87ab37e3034660f52da65e33ede31f3d1797d1
Elijah Insua <tmpvar@gmail.com>
2013-03-01 22:25:02 -0700

    make offset-split-negative pass

--- offset.js
+++ offset.js
@@ -67,7 +67,7 @@ module.exports = function(poly, delta, cornerFn) {
 
   var f = [], poly = Polygon(ret), seen = {};
   poly.each(function(prev, current) {
-    f.push(prev);
+    //f.push(prev);
     poly.each(function(ip, ic) {
       var i = segseg(prev, current, ip, ic);
 
@@ -79,6 +79,7 @@ module.exports = function(poly, delta, cornerFn) {
 
         var key = i.x + ':'+ i.y;
         if (!seen[key]) {
+          i.intersection = true;
           f.push(i);
           seen[key] = true;
 
@@ -91,7 +92,7 @@ module.exports = function(poly, delta, cornerFn) {
     f.push(current);
   });
 
-  return [Polygon(f.filter(function(current) {
+  f = f.filter(function(current) {
 
     var contained = orig.containsPoint(current);
     if ((delta > 0 && contained) || (delta < 0 && !contained)) {
@@ -103,5 +104,56 @@ module.exports = function(poly, delta, cornerFn) {
       return false;
     }
     return true;
-  })).clean().points];
+  });
+
+  function Node(vec) {
+    this.vec = vec;
+    this.children = [];
+  }
+
+  Node.prototype = {
+    vec : null,
+    children: null
+  };
+
+  // final polygon detection
+  var polys = [], poly = [], last = null;
+
+  var sorted = f.concat();
+  sorted.sort(function(a, b) {
+    return (a.y < b.y) ? -1 : 1;
+  }).forEach(function(item) {
+    var s = Vec2(-1000, item.y), e = Vec2(1000, item.y);
+    var count = 0;
+    Polygon(f).each(function(p, c) {
+      var i = segseg(s, e, p, c);
+      if (i && i!==true) {
+        count++;
+      }
+    });
+
+    item.beamCount = count;
+
+    console.log('SCANBEAM @', item.y, '#', count, !!item.intersection);
+  });
+
+  last = f[0].beamCount;
+  Polygon(f).each(function(p, c, n) {
+    console.log('ITEM', c.beamCount, c.intersection);
+    if (c.beamCount%2 === 1 && !c.intersection && poly.length > 0) {
+      poly.push(c);
+      poly.length && polys.push(poly);
+      poly = [];
+    } else {
+      poly.push(c);
+    }
+    last = c.beamCount;
+  });
+
+  polys.push(poly);
+  polys.forEach(function(p) {
+    console.log(p.length);
+  })
+console.log(polys.length)
+  return polys;
 };
